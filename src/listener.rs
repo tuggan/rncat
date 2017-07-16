dfa156394ce222cd55b824a0ca3ea85e7ff01057
Dennis Vesterlund <dennisvesterlund@gmail.com>
2017-07-16 09:17:34 +0200

    Added getopts and moved listening logic to a new file.

---
+++ src/listener.rs
@@ -0,0 +1,39 @@
+use std::net::{TcpStream, TcpListener};
+
+use std::io::Read;
+use std::io::Write;
+use std::io::stdout;
+
+pub fn print_incomming(mut stream: TcpStream) {
+    let mut buf;
+    loop {
+        buf = [0; 512];
+        let _ = match stream.read(&mut buf) { 
+            Err(e) => panic!("Failed to read stream: {}", e),
+            Ok(m) => {
+                if m == 0 {
+                    break;
+                }
+                m
+            }
+        };
+        match stdout().write(&buf) {
+            Err(e) => panic!("Failed while writing to stdout: {}", e),
+            Ok(s) => s,
+        };
+    }
+}
+
+pub fn listen(adress: &str, port: &str) {
+    match TcpListener::bind(format!("{}:{}", adress, port)) {
+        Err(e) => panic!("Error while binding to adress {}:{}: {}", adress, port, e),
+        Ok(l) => {
+            for stream in l.incoming() {
+                match stream {
+                    Err(e) => println!("failed: {}", e),
+                    Ok(stream) => print_incomming(stream),
+                }
+            }
+        }
+    };
+}
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,49 @@
-use std::net::{TcpListener, TcpStream};
+extern crate getopts;
 
-use std::io::Read;
-use std::io::Write;
-use std::io::stdout;
+use getopts::Options;
+use std::env;
 
+mod listener;
 
-fn print_incomming(mut stream: TcpStream) {
-    let mut buf;
-    loop {
-        buf = [0; 512];
-        let _ = match stream.read(&mut buf) { 
-            Err(e) => panic!("Got an error {}", e),
-            Ok(m) => {
-                if m == 0 {
-                    break;
-                }
-                m
-            }
-        };
-        match stdout().write(&buf) {
-            Err(e) => panic!("Got error while writing to stdout: {}", e),
-            Ok(s) => println!("Wrote {} bytes", s),
-        };
-    }
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} FILE [options]", program);
+    print!("{}", opts.usage(&brief));
 }
 
-
 fn main() {
-    match TcpListener::bind("127.0.0.1:8888") {
-        Err(e) => panic!("Error while binding to adress: {}", e),
-        Ok(l) => {
-            for stream in l.incoming() {
-                match stream {
-                    Err(e) => println!("failed: {}", e),
-                    Ok(stream) => print_incomming(stream),
-                }
-            }
-        }
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+    let mut listener: bool = false;
+    let mut adress = String::from("0.0.0.0");
+    let mut port = String::from("8888");
+
+    let mut opts = Options::new();
+
+    opts.optflag("l", "listen", "Listen on port ");
+    opts.optflag("h", "help", "Print this help menu");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(e) => panic!("  {}", e),
     };
+
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
+
+    if matches.opt_present("l") {
+        println!("Server mode enabled!");
+        listener = true;
+    }
+
+    if matches.free.len() == 2 {
+        adress = matches.free[0].clone();
+        port = matches.free[1].clone();
+    };
+
+    if listener {
+        listener::listen(&adress, &port);
+    } else {
+
+    }
 }
